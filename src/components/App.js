a86ca62bd11efabdca0af817eea28e01ae3a6ce7
Victor Perez <vicperez@cobogpgp3428.global.publicisgroupe.net>
2017-11-12 22:35:49 -0500

    All together, store, reducer, actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
+import { addRecipe } from '../actions'
 
 class App extends Component {
+  state = {
+    calendar: null,
+  }
+
+  componentDidMount() {
+    const { store } = this.props
+
+    store.subscribe(() => {
+      this.setState(() => ({
+        calendar: store.getState(),
+      }))
+    })
+  }
+
+  submitFood = () => {
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      meal: 'breakfast',
+      recipe: {
+        label: this.input.value,
+      },
+    }))
+
+    this.input.value = ''
+  }
   render() {
     return (
       <div>
-        Hello World
+        <input
+          type="text"
+          ref={(input) => this.input = input}
+          placeholder="Monday's breakfast"
+        />
+
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     )
   }
