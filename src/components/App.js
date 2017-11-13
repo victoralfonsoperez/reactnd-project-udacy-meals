98a827094bc5155363beb7f55da0d2acd9b4b101
Victor Perez <vicperez@cobogpgp3428.global.publicisgroupe.net>
2017-11-13 09:28:03 -0500

    mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
+  doThing = () => {
+    this.props.selectRecipe({})
+  }
   render() {
     console.log('Props', this.props)
     return (
@@ -29,4 +33,11 @@ function mapStateToProps(calendar) {
   }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: data => dispatch(addRecipe(data)),
+    remove: data => dispatch(removeFromCalendar(data)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
